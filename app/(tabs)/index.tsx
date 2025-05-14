0145a3dfe724ed6d48f0721e343b7d4c93c967be
Running water <alsgks1218@naver.com>
2025-05-14 17:35:32 +0900

    공통 버튼 임시로 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
 import { SafeAreaView, Text } from "react-native";
 
+import CustomButton from "@/components/custom-button";
+
 export default function Page() {
   return (
     <SafeAreaView>
       <Text>홈</Text>
+      <CustomButton label="텍스트" />
     </SafeAreaView>
   );
 }
