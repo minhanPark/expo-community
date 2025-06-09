2529dd7d82e186d082a0438ec74e97b0a369360f
minhanPark <alsgks1218@naver.com>
2025-06-09 15:29:53 +0900

    push noti hook 코드 문서랑 비교 후 일단 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,20 @@ import { useEffect } from "react";
 import Toast from "react-native-toast-message";
 // 기본적으로 제공되는 ActionSheet는 ios에서만 있어서 공통적으로 사용할 수 있는 action sheet를 설치
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
+import * as Notifications from "expo-notifications";
+
+// expo-notifications는 사용자의 권한을 요청하고 푸시 알림을 전송하기 위한 엑스포 푸시 토큰을 얻는데 사용됨
+// expo-device는 앱이 실제 기기에서 실행 중인지 확인하는 데 사용
+// expo-constants는 앱 구성에서 projectId 값을 가져오는데 사용
+// 레이아웃에 docs에서 갖고온 setNotificationHandler을 넣어준다.
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
 
 export default function RootLayout() {
   useReactQueryDevTools(queryClient);
